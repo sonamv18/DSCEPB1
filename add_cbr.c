e1587aa2728b5ac7282a18c278078e62210425bb
sonamv18 <49298596+sonamv18@users.noreply.github.com>
2019-04-20 14:57:31 +0530

    Update add_cbr.c

--- add_cbr.c
+++ add_cbr.c
@@ -1,28 +1,27 @@
 
-   #include<stdio.h>
- 2 void get_input(int *a)
- 3     {
- 4        printf("Enter a number:");
- 5        scanf("%d",a);
- 6
- 7     }
- 8
- 9 void addone(int a,int *b)
-10     {
-11        *b= a+1;
-12     }
-13
-14 void output(int *a,int b)
-15     {
-16       printf("%n +1 is %d", a,b);
-17     }
-18 int main()
-19     {
-20       int *a,c,*b;
-21       a=&c;
-22       get_input(*a)
-23       addone( a,&b);
-24       output(  &a, b );
-25     }
-26
-27
+#include<stdio.h>
+void get_input(int *a)
+    {
+       printf("Enter a number:");
+       scanf("%d",a);
+
+    }
+
+void addone(int *a)
+    {
+       *a=*a+1;
+    }
+
+void output(int *a)
+    {
+      printf("%d+1 is %d", a,*a);
+    }
+int main()
+    {
+      int *a,c;
+      a=&c;
+      get_input(a);
+      addone(a);
+      output(a);
+    }
+
