36c984e4b0c5b261c8cfda53f8210294794e44a7
sonamv18 <49298596+sonamv18@users.noreply.github.com>
2019-04-20 12:19:54 +0530

    do i need to initialize c too?

--- add_cbr.c
+++ add_cbr.c
@@ -1,25 +1,28 @@
-#include<stdio.h>
-void get_input(int *a)
-    {
-       printf("Enter a number:");
-       scanf("%d",a);
 
-    }
+  #include<stdio.h>
+  void get_input(int *a)
+      {
+         printf("Enter a number:");
+         scanf("%d",a);
+ 
+      }
+ 
+  void addone(int a,int *b)
+     {
+        *b= a+1;
+     }
 
-void addone(int a,int *b)
-    {
-       *b= a+1;
-    }
-
-void output(int *a,int b)
-    {
-      printf("%n +1 is %d", a,b);
-    }
+ void output(int *a,int b)
+     {
+       printf("%n +1 is %d", a,b);
+     }
 int main()
-    {
-      int *a,*b;
-      get_input(&a);
-      addone( a,&b);
-      output(  &a, b );
-    }
+     {
+       int *a,*b;
+       *a=&c
+       get_input(a);
+       addone( a,&b);
+       output( &a , b);
+       }
+
 
