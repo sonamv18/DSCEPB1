6557e12295b92f13895b18106239a3bca27e39d0
sonamv18 <49298596+sonamv18@users.noreply.github.com>
2019-04-20 10:51:47 +0530

    Update addone.c

--- addone.c
+++ addone.c
@@ -1,27 +1,32 @@
-#include <stdio.h>
-int get_input();
-{
-    int a;
-    printf("Enter a number: ");
-    scanf("%d",&a);
-    return a;
-}
+ 
+#include<stdio.h>
+int get_input()
+    {
+       int a;
+       printf("Enter a number:");
+       scanf("%d",&a);
+       return a;
+    }
 
-int add_one(int a)
-{
-    b=a+1;
-    return b;
-}
+int addone(int a)
+    {
+       int b= a+1;
+       return b;
+    }
 
-void output(int a, int b)
-{
-    printf("%d + 1 is %d",a,b);
-}
+void output(int a,int b)
+    {
+      printf("%d +1 is %d", a,b);
+    }
 
-int main(void)
-{
-    int a=get_input();
-    int b=add_one(a);
-    output(a,b);
-}
+int main()
+    {
+      int a=get_input();
+      int b=addone( a);
+      output(  a, b );
+    }
+
+
+
+
 
