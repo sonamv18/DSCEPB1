479d8c29e7e288274104c28b3aeaae3ba22f92f8
sonamv18 <49298596+sonamv18@users.noreply.github.com>
2019-05-14 09:23:28 +0530

    Update sumn.c

--- sumn.c
+++ sumn.c
@@ -2,37 +2,50 @@
 
 int input()
     {
-        int n,a[1000];
+        int n;
         printf("enter the n value");
         scanf("%d",&n);
-        for(i=0;i<n;i++)
+        return n;
+    }
+
+int inputa(a[size])
+    {
+        int *n,i;
+        printf("enter the elements of array");
+        for(i=0;i< n;i++)
         {
-             scanf("%d",&a[i]);
+             scanf(" %d",a[i]);
+
         }
-        return a[];
-     }
+
+    }
 int compute()
     {
          int i, sum==0;
-         for(i=o;i<n;i++)
+         for(i=0;i<n;i++)
          {
              sum=sum+a[i];
          }
          return sum;
     }
 
-int output()
+int output(a[size])
      {
-         int i
-
-
-
+         int i;
+         for(i=0;i<&n;i++)
+         {
+               printf("%d",a[i]);
+         }
+      }
 
 int main()
     {
-         input();
-         int a[1000]=
-        output(a[]);
+         int size=&n,a[size];
+         input(a[size]);
+         output(a[size]);
+   compute(a[size]);
     }
+
+
 
 
