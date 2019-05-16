80e53449dc3ff34a91fef6d0cbd9dc3919d5e676
sonamv18 <49298596+sonamv18@users.noreply.github.com>
2019-05-16 09:34:36 +0530

    Update sumn.c

--- sumn.c
+++ sumn.c
@@ -1,49 +1,42 @@
-
 #include<stdio.h>
 
-int input()
+
+float input()
     {
-        int n;
-        printf("enter the n value");
-        scanf("%d",&n);
-        return n;
+         float num,denom;
+         printf("enter the numerator of the fraction");
+         scanf("%d", &num);
+         printf("enter the denominator of the fraction");
+         scanf("%d",&denom);
+         float frac=num/denom;
+         return frac;
     }
 
-int inputa(int a[],int N)
+float compute( float frac1, float frac2)
     {
-        int i;
-        printf("enter the elements of array");
-        for(i=0;i< N;i++)
-        {
-             scanf(" %d",&a[i]);
 
-        }
-        return -1;
-    }
-int compute(int a[], int N)
-    {
-         int i, sum=0;
-         for(i=0;i<N;i++)
-         {
-             sum=sum+a[i];
-         }
+         float sum=0;
+         sum=frac1+frac2;
          return sum;
-    }
+     }
 
-int output(int res)
-     {
-         printf("the sum is %d",res);
-         return -1;
-      }
+void output(float sum)
+    {
+
+        printf("the sum is %f",sum);
+    }
 
 int main()
     {
-         int N=input();
-         int a[N];
-         inputa(a,N);
-         int res=compute(a,N);
-         output(res);
+         float frac1=input();
+         float frac2=input();
+         float sum=compute(frac1,frac2);
+         output(sum);
+         return -1;
     }
+
+
+
 
 
 
