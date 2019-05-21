7ff0898a25ff05657a46ba9ab57c8681bcb78bbd
sonamv18 <49298596+sonamv18@users.noreply.github.com>
2019-05-21 09:31:02 +0530

    Update addfrac.c

--- addfrac.c
+++ addfrac.c
@@ -1,37 +1,89 @@
 #include<stdio.h>
 
 
-float input()
+int input_nm()
     {
-         float num,denom;
+         int num;
          printf("enter the numerator of the fraction");
-         scanf("%f", &num);
-         printf("enter the denominator of the fraction");
-         scanf("%f",&denom);
-         float frac=num/denom;
-         return frac;
+         scanf("%d", &num);
+         return num;
     }
 
-float compute( float frac1, float frac2)
+int inpt_dn()
     {
+         int den;
+         printf("enter the denominator of the fraction");
+         scanf("%d",&den);
+         return den;
+    }
 
-         float sum=0;
-         sum=frac1+frac2;
-         return sum;
-     }
-
-void output(float sum)
+int gcd(int rnum, int cd)
     {
+         int gcd=1;
+         if(rnum%gcd=0&&cd%gcd=0)
+         gcd++;
+         else
+         return gcd;
+    }
 
-        printf("the sum is %f",sum);
+int compute_d(int den1,int den2)
+    {
+         int cd=(den1*den2);
+         return cd;
+    }
+int compute_n(int num1,int num2)
+    {
+      int rnum=(num1*den2)+(num2*den1);
+      retrn rnum;
+    }
+
+void output(int num1,int num2,int den1,int den2,int rnum,int cd,int gcd)
+    {
+        int rn=rnum/gcd;
+        int cd=cd/gcd;
+        printf("the sum of %d/%d + %d/%d = %d/%d", num1,den1,num2,den2,rn,cd );
+    }
     }
 
 int main()
     {
-         float frac1=input();
-         float frac2=input();
-         float sum=compute(frac1,frac2);
-         output(sum);
+         int num1=input_nm();
+         int num2=input_nm();
+         int den1=input_dn();
+         int den2=input_dn();
+         int rnum= compute_n(num1,num2);
+         int rden=compute_d(den1,den2);
+         int gcd=gcd(rnum,rden);
+         output(num1,num2,den1,den2,rnum,rden,gcd);
          return -1;
     }
+
+    }
+
+int main()
+    {
+         int num1=input_nm();
+         int num2=input_nm();
+         int den1=input_dn();
+         int den2=input_dn();
+         int rnum= compute_n(num1,num2);
+         int rden=compute_d(den1,den2);
+         int gcd=gcd(rnum,rden);
+         output(num1,num2,den1,den2,rnum,rden,gcd);
+         return -1;
+    }
+
+int main()
+    {
+         int num1=input_nm();
+         int num2=input_nm();
+         int den1=input_dn();
+         int den2=input_dn();
+         int rnum= compute_n(num1,num2);
+         int rden=compute_d(den1,den2);
+         int gcd=gcd(rnum,rden);
+         output(num1,num2,den1,den2,rnum,rden,gcd);
+         return -1;
+    }
+
 
