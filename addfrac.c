16f13a7731e2b3ab358ef8566203e517ade999aa
sonamv18 <49298596+sonamv18@users.noreply.github.com>
2019-05-22 09:21:26 +0530

    Update addfrac.c

--- addfrac.c
+++ addfrac.c
@@ -17,73 +17,29 @@ int inpt_dn()
          return den;
     }
 
-int gcd(int rnum, int cd)
-    {
-         int gcd=1;
-         if(rnum%gcd=0&&cd%gcd=0)
-         gcd++;
-         else
-         return gcd;
-    }
-
 int compute_d(int den1,int den2)
     {
          int cd=(den1*den2);
          return cd;
     }
-int compute_n(int num1,int num2)
+int compute_n(int num1,int num2,int den1,int den2)
     {
       int rnum=(num1*den2)+(num2*den1);
-      retrn rnum;
+      return rnum;
     }
 
-void output(int num1,int num2,int den1,int den2,int rnum,int cd,int gcd)
+void output(int num1,int num2,int den1,int den2,int rnum,int cd)
     {
-        int rn=rnum/gcd;
-        int cd=cd/gcd;
-        printf("the sum of %d/%d + %d/%d = %d/%d", num1,den1,num2,den2,rn,cd );
-    }
+        printf("the sum of %d/%d + %d/%d = %d/%d", num1,den1,num2,den2,rnum,cd );
     }
 
 int main()
     {
          int num1=input_nm();
          int num2=input_nm();
-         int den1=input_dn();
-         int den2=input_dn();
-         int rnum= compute_n(num1,num2);
+         int den1=inpt_dn();
+         int den2=inpt_dn();
+         int rnum= compute_n(num1,num2,den1,den2);
          int rden=compute_d(den1,den2);
-         int gcd=gcd(rnum,rden);
-         output(num1,num2,den1,den2,rnum,rden,gcd);
+         output(num1,num2,den1,den2,rnum,rden);
          return -1;
-    }
-
-    }
-
-int main()
-    {
-         int num1=input_nm();
-         int num2=input_nm();
-         int den1=input_dn();
-         int den2=input_dn();
-         int rnum= compute_n(num1,num2);
-         int rden=compute_d(den1,den2);
-         int gcd=gcd(rnum,rden);
-         output(num1,num2,den1,den2,rnum,rden,gcd);
-         return -1;
-    }
-
-int main()
-    {
-         int num1=input_nm();
-         int num2=input_nm();
-         int den1=input_dn();
-         int den2=input_dn();
-         int rnum= compute_n(num1,num2);
-         int rden=compute_d(den1,den2);
-         int gcd=gcd(rnum,rden);
-         output(num1,num2,den1,den2,rnum,rden,gcd);
-         return -1;
-    }
-
-
