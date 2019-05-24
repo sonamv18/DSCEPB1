aba5f185d3285997476936d360f4db45a1b31c10
sonamv18 <49298596+sonamv18@users.noreply.github.com>
2019-05-24 09:13:56 +0530

    Update addfrac.c

--- addfrac.c
+++ addfrac.c
@@ -1,25 +1,12 @@
 #include<stdio.h>
 
 
-int input()
+int input(int *num1,int *den1)
     {
-         int i,value;
-         for(i=1;i<5;i++)
-             {
-                 if(i%2==0)
-                  {
-                      printf("enter the numerator of the fraction");
-                      scanf("%d", &value);
-                  }
-                 else
-                  {
-                      printf("enter the denominator of the fraction");
-                      scanf("%d",&value);
-                  }
-                 return value;
-              }
-           return -1;
-     }
+        printf("enter nmerator and denominator\n");
+        scanf("%d %d",num1,den1);
+        return 0;
+    }
 int compute_d(int den1,int den2)
     {
          int cd=(den1*den2);
@@ -38,15 +25,12 @@ void output(int num1,int num2,int den1,int den2,int rnum,int cd)
 
 int main()
     {
-         int num1=input();
-         int den1=input();
-         int num2=input();
-         int den2=input();
+         int num1=0,den1=0,num2=0,den2=0;
+         input(&num1,&den1);
+         input(&num2,&den2);
          int rnum= compute_n(num1,num2,den1,den2);
          int rden=compute_d(den1,den2);
          output(num1,num2,den1,den2,rnum,rden);
          return -1;
-     }
-
-
+    }
 
